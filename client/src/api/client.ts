de0fc3066be3c9dbe47f8f2acbdb25af8afc5bf9
sanket wani <sanketwani810@gmail.com>
2026-08-26 16:04:52 +0530

    new one

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || "https://human-resource-management-system-1hji3.onrender.com/api",
   headers: {
     'Content-Type': 'application/json',
   },
